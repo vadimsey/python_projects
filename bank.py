36221752ad7f5e00dfee6a5d0f9dfd0de2cd4910
vadimsey <allviwersh@gmail.com>
2025-12-28 15:31:36 +0300

    BANK OOP

--- bank.py
+++ bank.py
@@ -1,74 +1,146 @@
 import json
 
-def main():
-    choice()
+class BankAccount:
 
-
-def choice():
-    print("1. Create a new account")
-    print("2. View a new accounts")
-
-    choice = input("Choice the number: ")
-    if choice == "1":
-        new_account()
-    if choice == "2":
-        view_accounts()
-
-def view_accounts():
-    with open("accounts.json", "r") as json_file:
-        data = json.load(json_file)
-    for n, b in data.items():
-        print(f"Name:{n}, Balance:{b}")
-
-
-def new_account():
-    l = []
-    try:
-        with open("accounts.json","r") as file:
-            data = json.load(file)
-            if len(data.keys()) == 3:
-                print("=====YOU HAVE 3 ACCOUNTS=====")
-                choice()
-    except:
-        data = []
-
-
-    print("You can create max 3 accounts")
-    create = input("Do you want to create another account? (y/n)")
-    if create == "y":
-        create_account()
-    if create == "n":
-        choice()
-
-
-def create_account():
-    account = input("Enter new account name: ")
-    while True:
-        try:
-            balance = int(input("Enter your balance: "))
-            if balance < 0:
-                print("Please enter a positive number")
+    def create_acc(self):
+        print('===CREATE ACC===')
+        while True:
+            name = input('Enter account name(no digit): ').strip()
+            if len(name) == 0 or name.isdigit():
+                print('=====Enter correct name=====')
             else:
-                pass
+                break
 
-        except:
-            print("====Please enter a number====")
-        else:
-            break
+        while True:
+            try:
+                while True:
+                    balance = int(input('Enter account balance: '))
+                    if balance < 0:
+                        print("=====ENTER CORRECT BALANCE=====")
+                    else:
+                        break
+            except:
+                print('=====Enter correct balance=====')
+            else:
+                break
 
-    try:
-        with open("accounts.json", "r") as file:
-            accounts = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
         accounts = {}
+        try:
+            with open("accounts.json", "r") as file:
+                accounts = json.load(file)
+        except:
+            accounts = {}
 
-    accounts[account] = balance
+        accounts[name] = balance
 
-    with open("accounts.json", "w") as file:
-        json.dump(accounts, file)
-
-    choice()
+        with open("accounts.json", "w") as file:
+            json.dump(accounts, file)
 
 
-if __name__ == "__main__":
+    def view_acc(self):
+        print('===VIEW ACC===')
+        try:
+            with open("accounts.json", "r") as file:
+                accounts = json.load(file)
+        except:
+            accounts = {}
+
+        for i, n in enumerate(accounts.items()):
+            print(f"{i + 1}. {n[0]} - {n[1]}")
+
+    def withdraw(self):
+        print('===WITHDRAW===')
+        try:
+            with open("accounts.json", "r") as file:
+                accounts = json.load(file)
+        except:
+            accounts = {}
+
+        while True:
+            name = input('Enter account name(no digit): ').strip()
+            if name not in accounts:
+                print("=====THIS ACCOUNT IS NOT VALID=====")
+            else:
+                break
+
+        balance = accounts[name]
+
+        while True:
+            amount = input('Enter amount to withdraw: ')
+            if not str(amount).isdigit():
+                print('=====Amount cannot be an integer=====')
+            elif int(amount) < 0:
+                print('=====Amount cannot be negative=====')
+            elif int(amount) > balance:
+                print('=====Amount cannot be greater than balance=====')
+            else:
+                break
+
+        accounts[name] = (balance - int(amount))
+        with open("accounts.json", "w") as file:
+            json.dump(accounts, file)
+
+    def deposit(self):
+        print('===DEPOSIT===')
+        try:
+            with open("accounts.json", "r") as file:
+                accounts = json.load(file)
+        except:
+            accounts = {}
+
+        while True:
+            name = input('Enter account name(no digit): ').strip()
+            if name not in accounts:
+                print("=====THIS ACCOUNT IS NOT VALID=====")
+            else:
+                break
+
+        while True:
+            try:
+                amount = int(input('Enter amount to deposit: '))
+                if amount < 0:
+                    print('=====Amount cannot be negative=====')
+            except:
+                print("===Invalid amount===")
+            else:
+                break
+
+        balance = accounts[name]
+        accounts[name] = balance + amount
+        with open("accounts.json", "w") as file:
+            json.dump(accounts, file)
+
+
+def main():
+    bank = BankAccount()
+
+
+    while True:
+        print("\n=====MAIN MENU====")
+        print("1. Create new account")
+        print("2. View accounts")
+        print("3. Deposit")
+        print("4. Withdraw")
+        print("5. Exit")
+
+        try:
+            choice = int(input('Enter choice: '))
+        except:
+            print("=====Enter correct choice====")
+
+        if choice == 1:
+            bank.create_acc()
+        elif choice == 2:
+            bank.view_acc()
+        elif choice == 3:
+            bank.deposit()
+        elif choice == 4:
+            bank.withdraw()
+        elif choice == 5:
+            exit()
+        else:
+            print("=====Enter correct choice====")
+
+
+if __name__ == '__main__':
     main()
